6fdb8a4de72ef1181da6bc6cadf1b5bba79b2360
Janek Kozicki <janek@kozicki.pl>
2005-03-25 18:42:01 +0000

    still renaming collision to interaction...

---
+++ yade/yade/src/plugins/Geometry/BoundingVolumeFactory/InteractionGeometrySet2AABBFactory/CollisionGeometrySet2AABBFactory.cpp
@@ -0,0 +1,57 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Olivier Galizzi                                 *
+ *   olivier.galizzi@imag.fr                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+#include "CollisionGeometrySet2AABBFactory.hpp"
+#include "InteractionGeometrySet.hpp"
+#include "AABB.hpp"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CollisionGeometrySet2AABBFactory::go(	const shared_ptr<InteractionGeometry>& cm,
+						shared_ptr<BoundingVolume>& bv,
+						const Se3r& 	)
+{
+	shared_ptr<InteractionGeometrySet> set = dynamic_pointer_cast<InteractionGeometrySet>(cm);
+	
+	shared_ptr<AABB> aabb = dynamic_pointer_cast<AABB>(bv);
+	
+	vector<shared_ptr<InteractionGeometry> >::iterator si = set->interactionGeometries.begin();
+	vector<shared_ptr<InteractionGeometry> >::iterator siEnd = set->interactionGeometries.end();
+	Vector3r max(-Mathr::MAX_REAL,-Mathr::MAX_REAL,-Mathr::MAX_REAL);
+	Vector3r min(Mathr::MAX_REAL,Mathr::MAX_REAL,Mathr::MAX_REAL);
+	for( ; si!=siEnd ; ++si)
+	{
+		max = max.maxVector((*si)->bv->max);
+		min = min.minVector((*si)->bv->min);
+	}
+	
+	aabb->center = (max+min)*0.5;
+	aabb->halfSize = (max-min)*0.5;
+	
+	aabb->min = min;
+	aabb->max = max;
+}
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ yade/yade/src/plugins/Geometry/BoundingVolumeFactory/InteractionGeometrySet2AABBFactory/CollisionGeometrySet2AABBFactory.hpp
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Olivier Galizzi                                 *
+ *   olivier.galizzi@imag.fr                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+#ifndef __COLLISIONGEOMETRYSET2AABBFACTORY_H__
+#define __COLLISIONGEOMETRYSET2AABBFACTORY_H__
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "BoundingVolumeFactory.hpp"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CollisionGeometrySet2AABBFactory : public BoundingVolumeFactory
+{
+	public : void go(	const shared_ptr<InteractionGeometry>& cm,
+				shared_ptr<BoundingVolume>& bv,
+				const Se3r& se3	);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+REGISTER_FACTORABLE(CollisionGeometrySet2AABBFactory);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endif // __COLLISIONGEOMETRYSET2AABBFACTORY_H__
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
