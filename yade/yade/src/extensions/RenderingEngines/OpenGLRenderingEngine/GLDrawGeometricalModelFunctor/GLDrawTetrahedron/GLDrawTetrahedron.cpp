516bf66cbebffd37172821dcc5df3bb7aae467cf
Janek Kozicki <janek@kozicki.pl>
2005-04-25 12:33:30 +0000

    better openGL tetrahedron display
    commets in Factorable about classess description
    
    
    M    yade/src/extensions/RenderingEngines/OpenGLRenderingEngine/GLDrawGeometricalModelFunctor/GLDrawTetrahedron/GLDrawTetrahedron.cpp
    M    yade/src/toolboxes/Libraries/Factory/Factorable.hpp
    M    yade/projects/kdevelop/yade.kdevelop

--- yade/yade/src/extensions/RenderingEngines/OpenGLRenderingEngine/GLDrawGeometricalModelFunctor/GLDrawTetrahedron/GLDrawTetrahedron.cpp
+++ yade/yade/src/extensions/RenderingEngines/OpenGLRenderingEngine/GLDrawGeometricalModelFunctor/GLDrawTetrahedron/GLDrawTetrahedron.cpp
@@ -41,25 +41,96 @@ void GLDrawTetrahedron::go(const shared_ptr<GeometricalModel>& gm, const shared_
 	}
 	else
 	{
+// 		glBegin(GL_TRIANGLE_STRIP);
+// 			glEnable(GL_LIGHTING); 
+// 			
+// 			glNormal3v (tet->v4);
+// 			glVertex3fv(tet->v4);
+// 			
+// 			glNormal3v (tet->v1);
+// 			glVertex3fv(tet->v1);
+// 			
+// 			glNormal3v (tet->v2);
+// 			glVertex3fv(tet->v2);
+// 			
+// 			
+// 			glNormal3v (tet->v3);
+// 			glVertex3fv(tet->v3);
+// 			
+// 			glNormal3v (tet->v4);
+// 			glVertex3fv(tet->v4);
+// 			
+// 			glNormal3v (tet->v1);
+// 			glVertex3fv(tet->v1);
+// 		glEnd();
+
+/*		glDisable(GL_CULL_FACE);
 		glBegin(GL_TRIANGLE_STRIP);
 			glEnable(GL_LIGHTING); 
+		
+
+				glVertex3fv(tet->v4);
 			
-			glNormal3v (tet->v4);
+			
+				glVertex3fv(tet->v1);			
+
+			
+			Vector3r n = (tet->v1-tet->v4).unitCross(tet->v2-tet->v4);
+			glNormal3v (n);
+				glVertex3fv(tet->v2);
+			
+			n = (tet->v2-tet->v1).unitCross(tet->v3-tet->v1);
+			glNormal3v (n);
+			
+				glVertex3fv(tet->v3);
+			n = (tet->v3-tet->v2).unitCross(tet->v4-tet->v2);
+			glNormal3v (n);
+			
+				glVertex3fv(tet->v4);
+			n = (tet->v4-tet->v3).unitCross(tet->v1-tet->v3);
+			glNormal3v (n);
+				glVertex3fv(tet->v1);
+		glEnd();*/
+		glDisable(GL_CULL_FACE);
+		glEnable(GL_LIGHTING);
+
+		Vector3r center = 0.25*(tet->v1+tet->v2+tet->v3+tet->v4);
+		
+		glBegin(GL_TRIANGLES);
+			Vector3r n = (tet->v1-tet->v4).unitCross(tet->v2-tet->v4);
+			Vector3r faceCenter = (tet->v4+tet->v1+tet->v2)/3.0;
+			if ((faceCenter-center).dot(n)<0)
+				n = -n;
+			glNormal3v (n);
 			glVertex3fv(tet->v4);
-			
-			glNormal3v (tet->v1);
 			glVertex3fv(tet->v1);
-			
-			glNormal3v (tet->v2);
 			glVertex3fv(tet->v2);
 			
-			glNormal3v (tet->v3);
+			n = (tet->v2-tet->v1).unitCross(tet->v3-tet->v1);
+			faceCenter = (tet->v1+tet->v2+tet->v3)/3.0;
+			if ((faceCenter-center).dot(n)<0)
+				n = -n;
+			glNormal3v (n);
+			glVertex3fv(tet->v1);
+			glVertex3fv(tet->v2);
 			glVertex3fv(tet->v3);
-			
-			glNormal3v (tet->v4);
+
+			n = (tet->v3-tet->v2).unitCross(tet->v4-tet->v2);
+			faceCenter = (tet->v2+tet->v3+tet->v4)/3.0;
+			if ((faceCenter-center).dot(n)<0)
+				n = -n;
+			glNormal3v (n);
+			glVertex3fv(tet->v2);
+			glVertex3fv(tet->v3);
 			glVertex3fv(tet->v4);
 			
-			glNormal3v (tet->v1);
+			n = (tet->v4-tet->v3).unitCross(tet->v1-tet->v3);
+			faceCenter = (tet->v3+tet->v4+tet->v1)/3.0;
+			if ((faceCenter-center).dot(n)<0)
+				n = -n;
+			glNormal3v (n);
+			glVertex3fv(tet->v3);
+			glVertex3fv(tet->v4);
 			glVertex3fv(tet->v1);
 		glEnd();
 	}
