2f1feb167b7d54ab0ab0f95a532a9489998a0c1d
Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>
2008-07-24 14:40:55 +0000

    Fix a problem resulting in VectSet (default)  and HashMap being used simultaneously for transient/persistent interactions in the same simulation (which for unknown reasons is not good).

--- pkg/dem/PreProcessor/TriaxialTest.cpp
+++ pkg/dem/PreProcessor/TriaxialTest.cpp
@@ -256,7 +256,8 @@ bool TriaxialTest::generate()
 	createActors(rootBody);
 	positionRootBody(rootBody);
 
-	rootBody->transientInteractions		= shared_ptr<InteractionContainer>(new InteractionHashMap);
+	//rootBody->persistentInteractions	= shared_ptr<InteractionContainer>(new InteractionHashMap);
+	//rootBody->transientInteractions		= shared_ptr<InteractionContainer>(new InteractionHashMap);
 	rootBody->physicalActions		= shared_ptr<PhysicalActionContainer>(new PhysicalActionVectorVector);
 	rootBody->bodies 			= shared_ptr<BodyContainer>(new BodyRedirectionVector);
 
