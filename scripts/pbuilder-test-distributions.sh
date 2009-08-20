61ab184bda06b179564a1e967f664f1d48b28c9b
Václav Šmilauer <eudoxos@arcig.cz>
2009-08-20 15:40:30 +0200

    Make the test script report what distribution failed (currently only karmic).

--- scripts/pbuilder-test-distributions.sh
+++ scripts/pbuilder-test-distributions.sh
@@ -2,7 +2,7 @@
 # grab latest trunk, try building packages for various distributions
 # see http://yade.wikia.com/wiki/DebianPackages for configuration of the pbuilder environment
 #
-set -x -e
+set -x
 TMP=/tmp/pbuild-yade-$$
 mkdir $TMP
 cd $TMP
@@ -11,10 +11,10 @@ cd trunk; scripts/debian-prep jaunty; cd .. # distro not important here
 dpkg-source -b -I trunk
 DSC=yade-`cat trunk/VERSION`_1.dsc
 for DIST in lenny squeeze hardy jaunty karmic; do
-	echo ======================================================================================================
-	echo ============================================= TESTING $DIST ==========================================
-	echo ======================================================================================================
+	echo ============================================= TESTING $DIST ===================================
+	echo ===============================================================================================
 	BASETGZ=/var/cache/pbuilder/$DIST.tgz
-	sudo pbuilder --build --basetgz $BASETGZ --debbuildopts "-j5 -Zlzma" $DSC
+	sudo pbuilder --build --basetgz $BASETGZ --debbuildopts "-j5 -Zlzma" $DSC  || FAILED="$FAILED $DIST"
 done
 rm -rf $TMP
+if [ '$FAILED' ]; then echo "Build failed for $FAILED"; exit 1; fi
