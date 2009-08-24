c0daa9f76240ac46f485075324c39971996c3bce
Anton Gladky <gladky.anton@gmail.com>
2009-08-24 13:51:33 +0200

    Some changes on one-click-install script

--- scripts/installOnUbuntu.sh
+++ scripts/installOnUbuntu.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 # This script works on Ubuntu 9.04, and possibly on new Debians.
-# It "compiles and installs" the latest YADE SVN-version.
+# It "compiles and installs" the latest YADE BZR-version.
 # The install is maximal: you will get almost all YADE functionality, even if you dont need it
 # Please, copy this script to the empty directory and start it.
 # 
 
 
-BASEDIR='./'
+BASEDIR='.'
 VERSION='trunk'
 sudo aptitude install scons libqt3-mt-dev qt3-dev-tools freeglut3-dev libboost-date-time-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev fakeroot dpkg-dev build-essential g++ libboost-iostreams-dev liblog4cxx10-dev python-dev libboost-python-dev ipython python-matplotlib libsqlite3-dev python-numeric python-tk gnuplot doxygen python-pygraphviz python-epydoc
-
-svn checkout https://svn.berlios.de/svnroot/repos/yade/trunk
+bzr checkout lp:yade
 mkdir $BASEDIR/build
-mkdir $BASEDIR/data
-cd $BASEDIR/trunk
-scons PREFIX=$BASEDIR/../build optimize=1 -j2 openmp=True features=log4cxx,python,openGL,GTS version=$VERSION
+mkdir $BASEDIR/build/data
+cd $BASEDIR/yade
+scons PREFIX=$BASEDIR/../build optimize=1 -j2 features=log4cxx,python,opengl,gts,openmp version=$VERSION linkStrategy=monolithic
 scons doc
-cd ..
-$BASEDIR/build/bin/yade-$VERSION yade-epydoc.py
-
+cd doc
+$BASEDIR/../../build/bin/yade-$VERSION yade-epydoc.py
 
