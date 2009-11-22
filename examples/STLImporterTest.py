6e1b73b90af3fe9f56d9917bd37c24bac7142e7e
Sergei Dorofeenko <sj2001@yandex.ru>
2009-11-22 18:59:02 +0300

    Adaptation examples/STLImporterTest.py for new viscoelastic classes.

--- examples/STLImporterTest.py
+++ examples/STLImporterTest.py
@@ -10,7 +10,7 @@ es = 0.3
 
 ## Import wall's geometry
 params=utils.getViscoelasticFromSpheresInteraction(10e3,tc,en,es)
-imported = utils.import_stl_geometry('baraban.stl',frictionAngle=frictionAngle,physParamsClass="SimpleViscoelasticBodyParameters",**params) # **params sets kn, cn, ks, cs
+imported = utils.import_stl_geometry('baraban.stl',frictionAngle=frictionAngle,materialClass="SimpleViscoelasticMat",**params) # **params sets kn, cn, ks, cs
 
 ## Spheres
 sphereRadius = 0.2
@@ -22,30 +22,20 @@ for i in xrange(nbSpheres[0]):
             x = (i*2 - nbSpheres[0])*sphereRadius*1.1
             y = j*sphereRadius*2.2
             z = (k*2 - nbSpheres[2])*sphereRadius*1.1
-            s=utils.sphere([x,y,z],sphereRadius,density=Density,frictionAngle=frictionAngle,physParamsClass="SimpleViscoelasticBodyParameters")
-            p=utils.getViscoelasticFromSpheresInteraction(s.phys['mass'],tc,en,es)
-            s.phys['kn'],s.phys['cn'],s.phys['ks'],s.phys['cs']=p['kn'],p['cn'],p['ks'],p['cs']
+            s=utils.sphere([x,y,z],sphereRadius,density=Density,frictionAngle=frictionAngle,materialClass="SimpleViscoelasticMat")
+            p=utils.getViscoelasticFromSpheresInteraction(s.state['mass'],tc,en,es)
+            s.mat['kn'],s.mat['cn'],s.mat['ks'],s.mat['cs']=p['kn'],p['cn'],p['ks'],p['cs']
             O.bodies.append(s)
 
 ## Timestep 
 O.dt=.2*tc
-
-## Initializers 
-O.initializers=[
-	## Create bounding boxes. They are needed to zoom the 3d view properly before we start the simulation.
-	BoundingVolumeMetaEngine([InteractingSphere2AABB(),InteractingFacet2AABB(),MetaInteractingGeometry2AABB()])
-	]
 
 ## Engines 
 O.engines=[
 	## Resets forces and momenta the act on bodies
 	BexResetter(),
 	## Associates bounding volume to each body.
-	BoundingVolumeMetaEngine([
-		InteractingSphere2AABB(),
-		InteractingFacet2AABB(),
-		MetaInteractingGeometry2AABB()
-	]),
+	BoundingVolumeMetaEngine([ InteractingSphere2AABB(), InteractingFacet2AABB() ]),
 	## Using bounding boxes find possible body collisions.
 	InsertionSortCollider(),
 	## Create geometry information about each potential collision.
@@ -54,9 +44,11 @@ O.engines=[
 		InteractingFacet2InteractingSphere4SpheresContactGeometry()
 	]),
 	## Create physical information about the interaction.
-	InteractionPhysicsMetaEngine([SimpleViscoelasticRelationships()]),
+	InteractionPhysicsMetaEngine([
+		Ip2_SimleViscoelasticMat_SimpleViscoelasticMat_SimpleViscoelasticPhys()
+	]),
     ## Constitutive law
-	ConstitutiveLawDispatcher([ef2_Spheres_Viscoelastic_SimpleViscoelasticContactLaw()]),
+	ConstitutiveLawDispatcher([ Law2_Spheres_Viscoelastic_SimpleViscoelastic() ]),
 	## Apply gravity
 	GravityEngine(gravity=[0,-9.81,0]),
 	## Cundall damping must been disabled!
@@ -66,7 +58,6 @@ O.engines=[
 ]
 
 from yade import qt
-#qt.Controller()
 qt.View()
 O.run()
 
