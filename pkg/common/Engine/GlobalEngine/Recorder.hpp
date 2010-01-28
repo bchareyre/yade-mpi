ad5cc4c73c3d525d8f525a9544999041263690b3
Anton Gladky <gladky.anton@gmail.com>
2010-01-28 15:22:42 +0100

    1. Convert Recorder class to YADE_CLASS_BASE_DOC_ATTRS

--- pkg/common/Engine/GlobalEngine/Recorder.hpp
+++ pkg/common/Engine/GlobalEngine/Recorder.hpp
@@ -1,12 +1,5 @@
 // 2009 © Václav Šmilauer <eudoxos@arcig.cz>
 #pragma once 
-
-/*! Engine periodically storing some data to (one) external file.
-
-In addition PeriodicEngine, it handles opening the file as needed.
-
-See PeriodicEngine for controlling periodiciy.
-*/
 #include<yade/pkg-common/PeriodicEngines.hpp>
 #include<fstream>
 #include<string>
@@ -16,19 +9,15 @@ class Recorder: public PeriodicEngine{
 		//! stream object that derived engines should write to
 		std::ofstream out;
 	public:
-		//! Name of file to save to; must not be empty.
-		std::string file;
-		//! Whether to delete current file contents, if any, when opening (false by default)
-		bool truncate;
-		Recorder(): truncate(false){}
 		virtual bool isActivated(Scene* rb){
 			if(PeriodicEngine::isActivated(rb)){
 				if(!out.is_open()) openAndCheck();
 				return true;}
 			return false;
 		}
-		// virtual void postProcessAttributes(bool);
-	REGISTER_CLASS_AND_BASE(Recorder,PeriodicEngine);
-	REGISTER_ATTRIBUTES(PeriodicEngine,(file)(truncate));
+		YADE_CLASS_BASE_DOC_ATTRDECL_CTOR_PY(Recorder,PeriodicEngine,"Engine periodically storing some data to (one) external file. In addition PeriodicEngine, it handles opening the file as needed. See PeriodicEngine for controlling periodiciy",
+		((std::string,file,,"Name of file to save to; must not be empty."))
+		((bool,truncate,false,"Whether to delete current file contents, if any, when opening (false by default)"))
+		,,);
 };
 REGISTER_SERIALIZABLE(Recorder);
