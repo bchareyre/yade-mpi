eb2f03443c8d3f0dd64628b8ecf86c5bb5945f68
Jerome Duriez <duriez@geo.hmg.inpg.fr>
2010-02-02 15:55:17 +0100

    Dplt de SimpleShear hors de attic

--- pkg/dem/PreProcessor/SimpleShear.cpp
+++ pkg/dem/PreProcessor/SimpleShear.cpp
@@ -337,7 +337,7 @@ string SimpleShear::GenerateCloud(vector<BasicSphere>& sphere_list,Vector3r lowe
 			if (!overlap)
 			{
 				sphere_list.push_back(s);
-				cout << "j'ai bien rajoute une sphere dans la liste" << endl;
+// 				cout << "j'ai bien rajoute une sphere dans la liste" << endl;
 				break;
 			}
 		}
@@ -396,5 +396,5 @@ std::pair<string,bool> SimpleShear::ImportCloud(vector<BasicSphere>& sphere_list
 
 
 
-YADE_REQUIRE_FEATURE(PHYSPAR);
+// YADE_REQUIRE_FEATURE(PHYSPAR);
 
