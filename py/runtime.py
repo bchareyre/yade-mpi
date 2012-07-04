394fe30ff6a9968500f2d51694534827dec29611
Anton Gladky <gladky.anton@gmail.com>
2012-07-04 19:19:39 +0200

    Fix handling ipython 0.13 version.

--- py/runtime.py
+++ py/runtime.py
@@ -11,8 +11,8 @@ try: # attempt to get numerical version
 except ValueError:
 	print 'WARN: unable to extract IPython version from %s, defaulting to 10'%(IPython.__version__)
 	ipython_version=10
-if ipython_version not in (10,11,12): # versions that we are able to handle, round up or down correspondingly
-	newipver=10 if ipython_version<10 else 11
+if ipython_version not in (10,11,12,13): # versions that we are able to handle, round up or down correspondingly
+	newipver=10 if ipython_version<10 else 13
 	print 'WARN: unhandled IPython version 0.%d, assuming 0.%d instead.'%(ipython_version,newipver)
 	ipython_version=newipver
 
