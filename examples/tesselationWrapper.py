684abe13275bc8508f25f704cbc292bd8b6fb221
Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>
2012-07-12 23:18:19 +0200

    Document Tesselation wrapper a bit more. Add a complete example script.

---
+++ examples/tesselationWrapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#2012 Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>
+"""Example usage of a TesselationWrapper object for getting microscale quantities."""
+
+tt=TriaxialTest()
+tt.generate("test.yade")
+O.load("test.yade")
+O.run(100,True)
+TW=TesselationWrapper()
+TW.triangulate()	#compute regular Delaunay triangulation, don’t construct tesselation
+TW.computeVolumes()	#will silently tesselate the packing, then compute volume of each Voronoi cell
+TW.volume(10)		#get volume associated to sphere of id 10
+TW.setState(0)		#store current positions internaly for later use as the "0" state
+O.run(100,True)		#make particles move a little (let's hope they will!)
+TW.setState(1)		#store current positions internaly in the "1" (deformed) state
+
+#Now we can define strain by comparing states 0 and 1, and average them at the particles scale
+TW.defToVtk("strain.vtk")
