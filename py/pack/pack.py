d937726db0a3dcb15861a5b3e180441bfbf99350
Anton Gladky <gladky.anton@gmail.com>
2012-12-03 17:03:57 +0100

    Fix pack.py as suggested by Kneib François.
    http://www.mail-archive.com/yade-dev@lists.launchpad.net/msg08412.html

--- py/pack/pack.py
+++ py/pack/pack.py
@@ -110,10 +110,10 @@ if not (noPredicate):
       userColor='color' in kw
       for clump in clumps:
         clump=[i+idsOffset for i in clump]
-        clumpIds.append(O.bodies.clump(clump)) # clump spheres with given ids together, creating the clump object as well
+        clumpIds.append(O.bodies.clump([ids[i] for i in clump])) # clump spheres with given ids together, creating the clump object as well
         # make all spheres within one clump a single color, unless color was specified by the user
         if not userColor:
-          for i in clump[1:]: O.bodies[i].shape.color=O.bodies[clump[0]].shape.color
+          for i in clump[1:]: O.bodies[ids[i]].shape.color=O.bodies[ids[clump[0]]].shape.color
       return ids+clumpIds
   
   SpherePack.toSimulation=SpherePack_toSimulation
