0346b593856c661c148e362cd061c1d9c276a9d9
Jan Stransky <jan.stransky@fsv.cvut.cz>
2013-02-28 22:14:12 +0100

    complete optparse->argparse, --generate-manpage should work

--- py/manpage.py
+++ py/manpage.py
@@ -5,12 +5,12 @@
 # Copied from the logilab.common.optik_ext module (GPL-licensed) and modified for yade.
 #
 
-import optparse,time
+import argparse,time
 
-class ManHelpFormatter(optparse.HelpFormatter):
+class ManHelpFormatter(argparse.HelpFormatter):
 	"""Format help using man pages ROFF format"""
-	def __init__(self,indent_increment=0,max_help_position=24,width=79,short_first=0):
-		optparse.HelpFormatter.__init__(self, indent_increment, max_help_position, width, short_first)
+	def __init__(self,indent_increment=0,max_help_position=24,width=79):
+		argparse.HelpFormatter.__init__(self, indent_increment, max_help_position, width)
 	def format_heading(self, heading):
 		return '.SH %s\n' % heading.upper()
 	def format_description(self, description):
@@ -27,11 +27,11 @@ class ManHelpFormatter(optparse.HelpFormatter):
 		return '''.IP "%s"
 %s
 ''' % (optstring, help)
-	def format_head(self, optparser, metadata, section=1):
-		pgm = optparser.get_prog_name()
+	def format_head(self, argparser, metadata, section=1):
+		pgm = argparser.prog
 		short_desc = self.format_short_description(pgm, metadata['short_desc'])
 		long_desc = self.format_long_description(pgm, metadata['long_desc'])
-		return '%s\n%s\n%s\n%s' % (self.format_title(pgm, section), short_desc, self.format_synopsis(optparser), long_desc)
+		return '%s\n%s\n%s\n%s' % (self.format_title(pgm, section), short_desc, self.format_synopsis(argparser), long_desc)
 	def format_title(self, pgm, section):
 		date = '-'.join([str(num) for num in time.localtime()[:3]])
 		return '.TH %s %s "%s" %s' % (pgm, section, date, pgm)
@@ -43,9 +43,9 @@ class ManHelpFormatter(optparse.HelpFormatter):
 	def _markup(self, txt):
 		"""Prepares txt to be used in man pages."""
 		return txt.replace('-', '\\-')
-	def format_synopsis(self, optparser):
-		synopsis = optparser.get_usage()
-		pgm=optparser.get_prog_name()                                    
+	def format_synopsis(self, argparser):
+		synopsis = argparser.usage
+		pgm=argparser.prog
 		if synopsis:                                                           
 			synopsis = synopsis.replace('Usage: %s '%pgm, '')                   
 			return '.SH SYNOPSIS\n.B %s\n%s\n' % (self._markup(pgm),synopsis)
@@ -77,12 +77,13 @@ Please report bugs to the project\'s bug tracker at
 ''' % (metadata['copyright'],metadata['author'],metadata['license'],metadata['bugtracker'],metadata['author'],metadata['author_contact'])
 
 
-def generate_manpage(optparser,metadata,fileOut,seealso=None,section=1):
-	"""generate a man page from an optparse parser"""
+def generate_manpage(argparser,metadata,fileOut,seealso=None,section=1):
+	"""generate a man page from an argparse parser"""
 	out=open(fileOut,'w')
 	formatter=ManHelpFormatter()
-	formatter.parser=optparser
-	out.write(formatter.format_head(optparser,metadata,section))
-	out.write(optparser.format_option_help(formatter))
+	formatter.parser=argparser
+	out.write(formatter.format_head(argparser,metadata,section))
+	#out.write(optparser.format_option_help(formatter))
+	out.write(argparser.format_help())
 	out.write(formatter.format_tail(metadata,seealso))
 
