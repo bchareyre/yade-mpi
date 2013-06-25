ba48d90c3da79d3d116e3d2e3eadf5ca527ffaa2
Francois Kneib <francois.kneib@gmail.com>
2013-06-25 10:02:13 +0200

    Lots of changes in the inelastic cohesive set of classes. - correct a lot of bugs and comment the inelastic cohesive law : pkg/dem/Law2_ScGeom6D_InelastCohFrictPhys_CohesionMoment - change some variables names and comments : pkg/dem/InelastCohFrictMat pkg/dem/InelastCohFrictPhys pkg/dem/Ip2_2xInelastCohFrictMat_InelastCohFrictPhys

--- pkg/dem/InelastCohFrictMat.hpp
+++ pkg/dem/InelastCohFrictMat.hpp
@@ -1,37 +1,50 @@
+/*************************************************************************
+*  Copyright (C) 2012 by Ignacio Olmedo nolmedo.manich@gmail.com         *
+*  Copyright (C) 2012 by François Kneib   francois.kneib@gmail.com       *
+*  This program is free software; it is licensed under the terms of the  *
+*  GNU General Public License v2 or later. See file LICENSE for details. *
+*************************************************************************/
+
+
 #pragma once
 
-#include "CohFrictMat.hpp"
+#include "../common/ElastMat.hpp"
 
-
-class InelastCohFrictMat : public CohFrictMat
+class InelastCohFrictMat : public FrictMat
 {
 	public :
 		virtual ~InelastCohFrictMat ();
 
 /// Serialization
-	YADE_CLASS_BASE_DOC_ATTRS_CTOR(InelastCohFrictMat,CohFrictMat,"",
-		((Real,eTT,0.0,,"Tension elasticity modulus"))
-		((Real,eTC,0.0,,"Compresion elasticity modulus"))
-		((Real,eB,0.0,,"bending elasticity modulus"))
-		((Real,gTw,0.0,,"twist elasticity modulus"))	
-		((Real,sigmaB,0.0,,"max. Bending stress"))
-		((Real,sigmaTw,0.0,,"max. Twist stress"))
-		((Real,creepT,0.0,,"Tension/compression creeping"))
-		((Real,creepB,0.0,,"bending creeping"))
-		((Real,creepTw,0.0,,"twist creeping"))
-		((Real,unloadT,0.0,,"Tension/compression plastic unload"))
-		((Real,unloadB,0.0,,"bending plastic unload"))
-		((Real,unloadTw,0.0,,"twist plastic unload"))
-		((Real,disElT,0.0,,"max elastic displacement on Traction"))
-		((Real,disElC,0.0,,"max elastic displacement on Compresion"))
-		((Real,epsilonMaxT,0.0,,"Maximal plastic strain Tension"))
-		((Real,epsilonMaxC,0.0,,"Maximal plastic strain compression"))
-		((Real,phiBMax,0.0,,"Maximal plastic bending strain"))
-		((Real,phiTwMax,0.0,,"Maximal plastic bending strain")),
+	YADE_CLASS_BASE_DOC_ATTRS_CTOR(InelastCohFrictMat,FrictMat,"",
+		((Real,tensionModulus,0.0,,"Tension elasticity modulus"))
+		((Real,compressionModulus,0.0,,"Compresion elasticity modulus"))
+		((Real,shearModulus,0.0,,"shear elasticity modulus"))
+		((Real,alphaKr,2.0,,"Dimensionless coefficient used for the rolling stiffness."))
+		((Real,alphaKtw,2.0,,"Dimensionless coefficient used for the twist stiffness."))
+
+		((Real,nuBending,0.0,,"Bending elastic stress limit"))
+		((Real,nuTwist,0.0,,"Twist elastic stress limit"))
+		((Real,sigmaTension,0.0,,"Tension elastic stress limit"))
+		((Real,sigmaCompression,0.0,,"Compression elastic stress limit"))
+		((Real,shearCohesion,0.0,,"Shear elastic stress limit"))
+		
+		((Real,creepTension,0.0,,"Tension/compression creeping coefficient. Usual values between 0 and 1."))
+		((Real,creepBending,0.0,,"Bending creeping coefficient. Usual values between 0 and 1."))
+		((Real,creepTwist,0.0,,"Twist creeping coefficient. Usual values between 0 and 1."))
+		
+		((Real,unloadTension,0.0,,"Tension/compression plastic unload coefficient. Usual values between 0 and +infinity."))
+		((Real,unloadBending,0.0,,"Bending plastic unload coefficient. Usual values between 0 and +infinity."))
+		((Real,unloadTwist,0.0,,"Twist plastic unload coefficient. Usual values between 0 and +infinity."))
+		
+		((Real,epsilonMaxTension,0.0,,"Maximal plastic strain tension"))
+		((Real,epsilonMaxCompression,0.0,,"Maximal plastic strain compression"))
+		((Real,etaMaxBending,0.0,,"Maximal plastic bending strain"))
+		((Real,etaMaxTwist,0.0,,"Maximal plastic twist strain")),
 		createIndex();			  
 					);
 /// Indexable
-	REGISTER_CLASS_INDEX(InelastCohFrictMat,CohFrictMat);
+	REGISTER_CLASS_INDEX(InelastCohFrictMat,FrictMat);
 };
 
 REGISTER_SERIALIZABLE(InelastCohFrictMat);
