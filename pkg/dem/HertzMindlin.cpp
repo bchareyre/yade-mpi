bd698e9a7aa9e05dfc7ead6293e3dd35a8f960d9
Klaus Thoeni <klaus.thoeni@gmail.com>
2013-09-08 05:40:06 +1000

    add MatchMaker for friction angle

--- pkg/dem/HertzMindlin.cpp
+++ pkg/dem/HertzMindlin.cpp
@@ -31,8 +31,8 @@ CREATE_LOGGER(Ip2_FrictMat_FrictMat_MindlinPhys);
 
 void Ip2_FrictMat_FrictMat_MindlinPhys::go(const shared_ptr<Material>& b1,const shared_ptr<Material>& b2, const shared_ptr<Interaction>& interaction){
 	if(interaction->phys) return; // no updates of an already existing contact necessary
-	shared_ptr<MindlinPhys> mindlinPhys(new MindlinPhys());
-	interaction->phys = mindlinPhys;
+	shared_ptr<MindlinPhys> contactPhysics(new MindlinPhys());
+	interaction->phys = contactPhysics;
 	FrictMat* mat1 = YADE_CAST<FrictMat*>(b1.get());
 	FrictMat* mat2 = YADE_CAST<FrictMat*>(b2.get());
 	
@@ -64,20 +64,20 @@ void Ip2_FrictMat_FrictMat_MindlinPhys::go(const shared_ptr<Material>& b1,const 
 	Real Rmean = (Da+Db)/2.; // mean radius
 	Real Kno = 4./3.*E*sqrt(R); // coefficient for normal stiffness
 	Real Kso = 2*sqrt(4*R)*G/(2-V); // coefficient for shear stiffness
-	Real frictionAngle = std::min(fa,fb);
+	Real frictionAngle = (!frictAngle) ? std::min(fa,fb) : (*frictAngle)(mat1->id,mat2->id,mat1->frictionAngle,mat2->frictionAngle);
 
 	Real Adhesion = 4.*Mathr::PI*R*gamma; // calculate adhesion force as predicted by DMT theory
 
 	/* pass values calculated from above to MindlinPhys */
-	mindlinPhys->tangensOfFrictionAngle = std::tan(frictionAngle); 
-	//mindlinPhys->prevNormal = scg->normal; // used to compute relative rotation
-	mindlinPhys->kno = Kno; // this is just a coeff
-	mindlinPhys->kso = Kso; // this is just a coeff
-	mindlinPhys->adhesionForce = Adhesion;
+	contactPhysics->tangensOfFrictionAngle = std::tan(frictionAngle); 
+	//contactPhysics->prevNormal = scg->normal; // used to compute relative rotation
+	contactPhysics->kno = Kno; // this is just a coeff
+	contactPhysics->kso = Kso; // this is just a coeff
+	contactPhysics->adhesionForce = Adhesion;
 	
-	mindlinPhys->kr = krot;
-	mindlinPhys->ktw = ktwist;
-	mindlinPhys->maxBendPl = eta*Rmean; // does this make sense? why do we take Rmean?
+	contactPhysics->kr = krot;
+	contactPhysics->ktw = ktwist;
+	contactPhysics->maxBendPl = eta*Rmean; // does this make sense? why do we take Rmean?
 
 	/* compute viscous coefficients */
 	if(en && betan) throw std::invalid_argument("Ip2_FrictMat_FrictMat_MindlinPhys: only one of en, betan can be specified.");
@@ -86,13 +86,13 @@ void Ip2_FrictMat_FrictMat_MindlinPhys::go(const shared_ptr<Material>& b1,const 
 	// en or es specified, just compute alpha, otherwise alpha remains 0
 	if(en || es){
 		Real logE = log((*en)(mat1->id,mat2->id));
-		mindlinPhys->alpha = -sqrt(5/6.)*2*logE/sqrt(pow(logE,2)+pow(Mathr::PI,2))*sqrt(2*E*sqrt(R)); // (see Tsuji, 1992)
+		contactPhysics->alpha = -sqrt(5/6.)*2*logE/sqrt(pow(logE,2)+pow(Mathr::PI,2))*sqrt(2*E*sqrt(R)); // (see Tsuji, 1992)
 	}
 	
 	// betan specified, use that value directly; otherwise give zero
 	else{	
-		mindlinPhys->betan=betan ? (*betan)(mat1->id,mat2->id) : 0; 
-		mindlinPhys->betas=betas ? (*betas)(mat1->id,mat2->id) : mindlinPhys->betan;
+		contactPhysics->betan=betan ? (*betan)(mat1->id,mat2->id) : 0; 
+		contactPhysics->betas=betas ? (*betas)(mat1->id,mat2->id) : contactPhysics->betan;
 	}
 }
 
