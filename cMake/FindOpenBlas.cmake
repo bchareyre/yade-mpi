2f26aff306e147581013328b4e5b4e03bd13d216
Anton Gladky <gladky.anton@gmail.com>
2014-02-28 10:40:54 +0100

    Fix library name for new openblas.

--- cMake/FindOpenBlas.cmake
+++ cMake/FindOpenBlas.cmake
@@ -1,10 +1,10 @@
 # - Find OpenBlas library
 # 
 # This module defines
-#  OPENBLAS_LIBRARY, libraries to link against to use GL2PS.
-#  OPENBLAS_FOUND, If false, do not try to use GL2PS.
+#  OPENBLAS_LIBRARY, libraries to link against to use Openblas.
+#  OPENBLAS_FOUND, If false, do not try to use Openblas.
 
-FIND_LIBRARY(OPENBLAS_LIBRARY NAMES openblas PATHS /usr/lib/openblas-base )
+FIND_LIBRARY(OPENBLAS_LIBRARY NAMES openblas blas PATHS /usr/lib/openblas-base )
 
 # handle the QUIETLY and REQUIRED arguments and set LOKI_FOUND to TRUE if
 # all listed variables are TRUE
