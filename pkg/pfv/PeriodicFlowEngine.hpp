f3e4e4581e81a815be936a679b21d4e41f5dad25
Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>
2014-04-17 17:08:50 +0200

    remove empty file

--- pkg/pfv/PeriodicFlowEngine.hpp
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
-*  Copyright (C) 2009 by Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>     *
-*  Copyright (C) 2012 by Donia Marzougui <donia.marzougui@grenoble-inp.fr>*
-*                                                                        *
-*  This program is free software; it is licensed under the terms of the  *
-*  GNU General Public License v2 or later. See file LICENSE for details. *
-*************************************************************************/
-
