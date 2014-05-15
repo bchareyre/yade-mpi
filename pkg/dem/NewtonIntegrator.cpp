1fc50be663733d97c67c73ee743f71e586189a83
Anton Gladky <gladky.anton@gmail.com>
2014-05-15 14:57:43 +0200

    Add mask parameter to NewtonIntegrator
    It is done to replace State.blockedMovement, which was
    discussed here
    http://www.mail-archive.com/yade-dev@lists.launchpad.net/msg10131.html
    
    Thanks, Bruno.

--- pkg/dem/NewtonIntegrator.cpp
+++ pkg/dem/NewtonIntegrator.cpp
@@ -226,7 +226,8 @@ void NewtonIntegrator::leapfrogTranslate(State* state, const Body::id_t& id, con
 	//NOTE : dVel defined without wraping the coordinates means bodies out of the (0,0,0) period can move realy fast. It has to be compensated properly in the definition of relative velocities (see Ig2 functors and contact laws).
 		//Reflect mean-field (periodic cell) acceleration in the velocity
 	if(scene->isPeriodic && homoDeform) {Vector3r dVel=dVelGrad*state->pos; state->vel+=dVel;}
-	if (not(state->blockedMovement)) {
+	
+	if ( (mask<=0) or ((mask>0) and ((Body::byId(id)->groupMask & mask)!=0)) ) {
 		state->pos+=state->vel*dt;
 	}
 }
@@ -234,12 +235,13 @@ void NewtonIntegrator::leapfrogTranslate(State* state, const Body::id_t& id, con
 void NewtonIntegrator::leapfrogSphericalRotate(State* state, const Body::id_t& id, const Real& dt )
 {
 	Real angle2=state->angVel.squaredNorm();
-	if (angle2!=0 and not(state->blockedMovement)) {//If we have an angular velocity, we make a rotation
+	if (angle2!=0 and ( (mask<=0) or ((mask>0) and ((Body::byId(id)->groupMask & mask)!=0)) )) {//If we have an angular velocity, we make a rotation
 		Real angle=sqrt(angle2);
 		Quaternionr q(AngleAxisr(angle*dt,state->angVel/angle));
 		state->ori = q*state->ori;
 	}
-	if(scene->forces.getMoveRotUsed() && scene->forces.getRot(id)!=Vector3r::Zero() and not(state->blockedMovement)) {
+	if(scene->forces.getMoveRotUsed() && scene->forces.getRot(id)!=Vector3r::Zero() 
+		and ( (mask<=0) or ((mask>0) and ((Body::byId(id)->groupMask & mask)!=0)) )) {
 		Vector3r r(scene->forces.getRot(id));
 		Real norm=r.norm(); r/=norm;
 		Quaternionr q(AngleAxisr(norm,r));
