0098c18afa817e2899c0997fd24e70b811e9a626
Bruno Chareyre <bruno.chareyre@hmg.inpg.fr>
2014-10-10 13:46:41 +0200

    split TwoPhaseFlowEngine in hpp/cpp for inclusion in child engines

--- pkg/pfv/TwoPhaseFlowEngine.cpp
+++ pkg/pfv/TwoPhaseFlowEngine.cpp
@@ -16,68 +16,9 @@
 #define TWOPHASEFLOW
 #ifdef TWOPHASEFLOW
 
-#include "FlowEngine_TwoPhaseFlowEngineT.hpp"
+#include "TwoPhaseFlowEngine.hpp"
 
-/// We can add data to the Info types by inheritance
-class TwoPhaseCellInfo : public FlowCellInfo_TwoPhaseFlowEngineT
-{
-	public:
-  	bool isWRes;
-	bool isNWRes;
-	bool isTrapW;
-	bool isTrapNW;
-	double saturation;
-	bool isImbibition;
-	double trapCapP;//for calculating the pressure of trapped phase, cell->info().p() = pressureNW- trapCapP. OR cell->info().p() = pressureW + trapCapP
-	std::vector<double> poreThroatRadius;
-	double poreBodyRadius;
-	double poreBodyVolume;
-	TwoPhaseCellInfo (void)
-	{
-		isWRes = true; isNWRes = false; isTrapW = false; isTrapNW = false;
-		saturation = 1.0;
-		isImbibition = false;
-		trapCapP = 0;
-		poreThroatRadius.resize(4, 0);
-		poreBodyRadius = 0;
-		poreBodyVolume = 0;
-	}
-	
-};
-
-class TwoPhaseVertexInfo : public FlowVertexInfo_TwoPhaseFlowEngineT {
-	public:
-	//same here if needed
-};
-
-typedef TemplateFlowEngine_TwoPhaseFlowEngineT<TwoPhaseCellInfo,TwoPhaseVertexInfo> TwoPhaseFlowEngineT;
-REGISTER_SERIALIZABLE(TwoPhaseFlowEngineT);
 YADE_PLUGIN((TwoPhaseFlowEngineT));
-
-class TwoPhaseFlowEngine : public TwoPhaseFlowEngineT
-{
-		double totalCellVolume;
-	public :
-	//We can overload every functions of the base engine to make it behave differently
-	//if we overload action() like this, this engine is doing nothing in a standard timestep, it can still have useful functions
-	virtual void action() {};
-	
-	//If a new function is specific to the derived engine, put it here, else go to the base TemplateFlowEngine
-	//if it is useful for everyone
-	void fancyFunction(Real what);
-
-	YADE_CLASS_BASE_DOC_ATTRS_INIT_CTOR_PY(TwoPhaseFlowEngine,TwoPhaseFlowEngineT,"documentation here",
-	((double,surfaceTension,0.0728,,"Water Surface Tension in contact with air at 20 Degrees Celsius is: 0.0728(N/m)"))
-	((bool,initialWetting,true,,"Initial wetting saturated (=true) or non-wetting saturated (=false)"))
-	((bool, isPhaseTrapped,true,,"If True, both phases can be entrapped by the other, which would correspond to snap-off. If false, both phases are always connected to their reservoirs, thus no snap-off."))
-	
-	,/*TwoPhaseFlowEngineT()*/,
-	,
-	.def("fancyFunction",&TwoPhaseFlowEngine::fancyFunction,(boost::python::arg("what")=0),"test function")
-	)
-	DECLARE_LOGGER;
-};
-REGISTER_SERIALIZABLE(TwoPhaseFlowEngine);
 YADE_PLUGIN((TwoPhaseFlowEngine));
 
 void TwoPhaseFlowEngine::fancyFunction(Real what) {std::cerr<<"yes, I'm a new function"<<std::endl;}
