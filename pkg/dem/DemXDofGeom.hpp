f6f00540c620a79afdfdba5d2a07f86395571bf7
jduriez <jerome.duriez@ucalgary.ca>
2014-12-10 17:06:47 -0700

    Typo in the doc (missing space)

--- pkg/dem/DemXDofGeom.hpp
+++ pkg/dem/DemXDofGeom.hpp
@@ -7,7 +7,7 @@
 	It might be removed in the future. */
 class GenericSpheresContact: public IGeom{
 	YADE_CLASS_BASE_DOC_ATTRS_CTOR(GenericSpheresContact,IGeom,
-		"Class uniting :yref:`ScGeom` and :yref:`L3Geom`, for the purposes of :yref:`GlobalStiffnessTimeStepper`. (It might be removed inthe future). Do not use this class directly.",
+		"Class uniting :yref:`ScGeom` and :yref:`L3Geom`, for the purposes of :yref:`GlobalStiffnessTimeStepper`. (It might be removed in the future). Do not use this class directly.",
 		((Vector3r,normal,,,"Unit vector oriented along the interaction, from particle #1, towards particle #2. |yupdate|"))
 		((Vector3r,contactPoint,,,"some reference point for the interaction (usually in the middle). |ycomp|"))
 		((Real,refR1,,,"Reference radius of particle #1. |ycomp|"))
