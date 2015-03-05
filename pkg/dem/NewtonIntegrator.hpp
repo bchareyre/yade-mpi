e739c8570ddafd5d0d7999e2072c104c9aa39178
Christian Jakob <jakob@ifgt.tu-freiberg.de>
2015-03-05 09:32:27 +0100

    insert link to user manual in documentation of NewtonIntegrator

--- pkg/dem/NewtonIntegrator.hpp
+++ pkg/dem/NewtonIntegrator.hpp
@@ -60,7 +60,7 @@ class NewtonIntegrator : public FieldApplier{
 		#endif
 		virtual void action();
 	YADE_CLASS_BASE_DOC_ATTRS_CTOR_PY(NewtonIntegrator,GlobalEngine,"Engine integrating newtonian motion equations.",
-		((Real,damping,0.2,,"damping coefficient for Cundall's non viscous damping (see [Chareyre2005]_) [-]"))
+		((Real,damping,0.2,,"damping coefficient for Cundall's non viscous damping (see `numerical damping <https://yade-dem.org/doc/formulation.html?highlight=damping#numerical-damping>`_ and [Chareyre2005]_)"))
 		((Vector3r,gravity,Vector3r::Zero(),,"Gravitational acceleration (effectively replaces GravityEngine)."))
 		((Real,maxVelocitySq,NaN,,"store square of max. velocity, for informative purposes; computed again at every step. |yupdate|"))
 		((bool,exactAsphericalRot,true,,"Enable more exact body rotation integrator for :yref:`aspherical bodies<Body.aspherical>` *only*, using formulation from [Allen1989]_, pg. 89."))
