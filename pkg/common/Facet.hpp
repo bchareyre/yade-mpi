d2bea940d64ff68cd3a1287574cf593af7b29029
Jan Stransky <jan.stransky@fsv.cvut.cz>
2015-06-06 14:45:58 +0200

    Added Facet.setVertices function to prevent memory leaks (see bug 1041084, #8)

--- pkg/common/Facet.hpp
+++ pkg/common/Facet.hpp
@@ -35,6 +35,8 @@ class Facet : public Shape {
 
 	void postLoad(Facet&);
 
+	void setVertices(const Vector3r& v0, const Vector3r& v1, const Vector3r& v2) { vertices[0]=v0; vertices[1]=v1; vertices[2]=v2; postLoad(*this); }
+
 	YADE_CLASS_BASE_DOC_ATTRS_CTOR_PY(Facet,Shape,"Facet (triangular particle) geometry.",
 		((vector<Vector3r>,vertices,vector<Vector3r>(3,Vector3r(NaN,NaN,NaN)),(Attr::triggerPostLoad | Attr::noResize),"Vertex positions in local coordinates."))
 		((Vector3r,normal,Vector3r(NaN,NaN,NaN),(Attr::readonly | Attr::noSave),"Facet's normal (in local coordinate system)"))
@@ -45,6 +47,7 @@ class Facet : public Shape {
 		#endif
 		,
 		/* ctor */ createIndex();,
+		.def("setVertices",&Facet::setVertices,"TODO")
 	);
 	DECLARE_LOGGER;
 	REGISTER_CLASS_INDEX(Facet,Shape);
