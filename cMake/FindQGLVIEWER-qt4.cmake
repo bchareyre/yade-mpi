01fea359c04d08f93d2feddcd07b31bc794dbb37
Anton Gladky <gladky.anton@gmail.com>
2016-05-19 22:51:22 +0200

    Split QGLViewer.cmake on qt4 and qt5 versions.
    Qt4 support will be completely removed after the next
    stable Yade release.

---
+++ cMake/FindQGLVIEWER-qt4.cmake
@@ -0,0 +1,24 @@
+# - Try to find QGLViewer
+# Once done this will define
+#
+#  QGLVIEWER_FOUND - system has QGLViewer
+#  QGLVIEWER_INCLUDE_DIR - the QGLViewer include directory
+#  QGLVIEWER_LIBRARIES - Link these to use QGLViewer
+
+find_path(QGLVIEWER_INCLUDE_DIR 
+          NAMES qglviewer.h
+          PATHS /usr/include/QGLViewer
+         )
+
+find_library(QGLVIEWER_LIBRARIES
+             NAMES QGLViewer QGLViewer-qt4
+)
+
+IF(QGLVIEWER_INCLUDE_DIR AND QGLVIEWER_LIBRARIES)
+   SET(QGLVIEWER_FOUND TRUE)
+   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DQGLVIEWER_FOUND")
+ENDIF(QGLVIEWER_INCLUDE_DIR AND QGLVIEWER_LIBRARIES)
+
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(QGLVIEWER-qt4 DEFAULT_MSG QGLVIEWER_INCLUDE_DIR QGLVIEWER_LIBRARIES)
