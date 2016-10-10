0acca1ea9b64d5ba22afc86660d535f6e69fb269
Bruno Chareyre <bruno.chareyre@grenoble-inp.fr>
2016-10-10 15:16:28 +0200

    list authors of the doc as in doi:10.5281/zenodo.34073

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -672,57 +672,48 @@ my_maketitle=r'''
 \vspace{5 mm}
 \vspace{70 mm}
 \begin{sffamily}\bfseries\Large
-V\'{a}clav \v{S}milauer, Emanuele Catalano, Bruno Chareyre, Sergei Dorofeenko, Jerome Duriez, Anton Gladky, Janek Kozicki, Chiara Modenese, Luc Scholt\`{e}s, Luc Sibille, Jan Str\'{a}nsk\'{y}, Klaus Thoeni
+V\'{a}clav \v{S}milauer, Emanuele Catalano, Bruno Chareyre, Sergei Dorofeenko, J\'er\^ome Duriez, Nolan Dyck, Jan Eli\'{a}\v{s}, Burak Er, Alexander Eulitz, Anton Gladky, Ning Guo, Christian Jakob, Fran\c{c}ois Kneib, Janek Kozicki, Donia Marzougui, Rapha\"el Maurin, Chiara Modenese, Luc Scholt\`{e}s, Luc Sibille, Jan Str\'{a}nsk\'{y}, Thomas Sweijen, Klaus Thoeni, Chao Yuan
 \end{sffamily}
 \vspace{20 mm}
 \hrule{}
 
 \vfill
 % Bottom of the page
-\textit{\Large Release '''\
+\textit{\Large 2nd Edition, after Release '''\
 +yade.config.revision\
 +r''', \today}
 \end{flushright}
 
 \end{titlepage}
 
-\text{\sffamily\bfseries\LARGE Authors}\\
-\\
-\text{\sffamily\bfseries\Large V\'{a}clav \v{S}milauer}\\
-\text{\sffamily\Large Freelance consultant (http://woodem.eu)}\\
-\\
-\text{\sffamily\bfseries\Large Emanuele Catalano}\\
-\text{\sffamily\Large Grenoble INP, UJF, CNRS, lab. 3SR}\\
-\\
-\text{\sffamily\bfseries\Large Bruno Chareyre}\\
-\text{\sffamily\Large Grenoble INP, UJF, CNRS, lab. 3SR}\\
-\\
-\text{\sffamily\bfseries\Large Sergei Dorofeenko}\\
-\text{\sffamily\Large IPCP RAS, Chernogolovka}\\
-\\
-\text{\sffamily\bfseries\Large Jerome Duriez}\\
-\text{\sffamily\Large Grenoble INP, UJF, CNRS, lab. 3SR}\\
-\\
-\text{\sffamily\bfseries\Large Anton Gladky}\\
-\text{\sffamily\Large TU Bergakademie Freiberg}\\
-\\
-\text{\sffamily\bfseries\Large Janek Kozicki}\\
-\text{\sffamily\Large Gdansk University of Technology - lab. 3SR Grenoble University }\\
-\\
-\text{\sffamily\bfseries\Large Chiara Modenese}\\
-\text{\sffamily\Large University of Oxford}\\
-\\
-\text{\sffamily\bfseries\Large Luc Scholt\`{e}s}\\
-\text{\sffamily\Large Grenoble INP, UJF, CNRS, lab. 3SR}\\
-\\
-\text{\sffamily\bfseries\Large Luc Sibille}\\
-\text{\sffamily\Large University of Nantes, lab. GeM}\\
-\\
-\text{\sffamily\bfseries\Large Jan Str\'{a}nsk\'{y}}\\
-\text{\sffamily\Large CVUT Prague}\\
-\\
-\text{\sffamily\bfseries\Large Klaus Thoeni}\\
-\text{\sffamily\Large The University of Newcastle (Australia)}\\
+\text{\sffamily\bfseries\Large Authors}
+\begin{multicols}{2}
+\begin{itemize}
+ \item \text{\sffamily\bfseries\normalsize V\'{a}clav \v{S}milauer}\\ \text{\sffamily\small Freelance consultant (http://woodem.eu)}
+ \item \text{\sffamily\bfseries\normalsize Emanuele Catalano}\\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR}
+ \item \text{\sffamily\bfseries\normalsize Bruno Chareyre}\\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR}
+ \item \text{\sffamily\bfseries\normalsize Sergei Dorofeenko}\\ \text{\sffamily\small IPCP RAS, Chernogolovka}
+ \item \text{\sffamily\bfseries\normalsize J\'er\^ome Duriez}\\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR}
+ \item \text{\sffamily\bfseries\normalsize Nolan Dyck}\\ \text{\sffamily\small Univ. of Western Ontario}
+ \item \text{\sffamily\bfseries\normalsize Jan Eliáš}\\ \text{\sffamily\small Brno University of Technology}
+ \item \text{\sffamily\bfseries\normalsize Burak Er}\\ \text{\sffamily\small Bursa Technical University}
+ \item \text{\sffamily\bfseries\normalsize Alexander Eulitz}\\ \text{\sffamily\small TU Berlin / Institute for Machine Tools}\\ \text{\sffamily\small and Factory Management}
+ \item \text{\sffamily\bfseries\normalsize Anton Gladky}\\ \text{\sffamily\small TU Bergakademie Freiberg}
+ \item \text{\sffamily\bfseries\normalsize Ning Guo}\\ \text{\sffamily\small Hong Kong Univ. of Science and Tech.}
+ \item \text{\sffamily\bfseries\normalsize Christian Jakob}\\ \text{\sffamily\small TU Bergakademie Freiberg}
+ \item \text{\sffamily\bfseries\normalsize François Kneib}\\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR / Irstea Grenoble}
+ \item \text{\sffamily\bfseries\normalsize Janek Kozicki}\\ \text{\sffamily\small Gdansk University of Technology}
+ \item \text{\sffamily\bfseries\normalsize Donia Marzougui}\\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR}
+ \item \text{\sffamily\bfseries\normalsize Raphaël Maurin}\\ \text{\sffamily\small Irstea Grenoble}
+ \item \text{\sffamily\bfseries\normalsize Chiara Modenese}\\ \text{\sffamily\small University of Oxford}
+ \item \text{\sffamily\bfseries\normalsize Luc Scholt\`{e}s}\\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR}
+ \item \text{\sffamily\bfseries\normalsize Luc Sibille}\\ \text{\sffamily\small University of Nantes, lab. GeM}
+ \item \text{\sffamily\bfseries\normalsize Jan Str\'{a}nsk\'{y}}\\ \text{\sffamily\small CVUT Prague}
+ \item \text{\sffamily\bfseries\normalsize Thomas Sweijen}\\ \text{\sffamily\small Utrecht University}
+ \item \text{\sffamily\bfseries\normalsize Klaus Thoeni}\\ \text{\sffamily\small The University of Newcastle (Australia)}
+ \item \text{\sffamily\bfseries\normalsize Chao Yuan} \\ \text{\sffamily\small Grenoble INP, UJF, CNRS, lab. 3SR}
+\end{itemize}
+\end{multicols}
 
 \text{\sffamily\bfseries\large Citing this document}\\
 In order to let users cite Yade consistently in publications, we provide a list of bibliographic references for the different parts of the documentation. This way of acknowledging Yade is also a way to make developments and documentation of Yade more attractive for researchers, who are evaluated on the basis of citations of their work by others. We therefore kindly ask users to cite Yade as accurately as possible in their papers, as explained in http://yade-dem/doc/citing.html.
