660c5d6ac9a2db90dc8a174012135337169964b4
Jan Stransky <jan.stransky@fsv.cvut.cz>
2016-10-18 11:04:27 +0200

    Changed L3Geom to ScGeom in tutorial example scripts

--- doc/sphinx/tutorial/02-gravity-deposition.py
+++ doc/sphinx/tutorial/02-gravity-deposition.py
@@ -21,9 +21,9 @@ O.engines=[
 	InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Facet_Aabb()]),
 	InteractionLoop(
 		# handle sphere+sphere and facet+sphere collisions
-		[Ig2_Sphere_Sphere_L3Geom(),Ig2_Facet_Sphere_L3Geom()],
+		[Ig2_Sphere_Sphere_ScGeom(),Ig2_Facet_Sphere_ScGeom()],
 		[Ip2_FrictMat_FrictMat_FrictPhys()],
-		[Law2_L3Geom_FrictPhys_ElPerfPl()]
+		[Law2_ScGeom_FrictPhys_CundallStrack()]
 	),
 	NewtonIntegrator(gravity=(0,0,-9.81),damping=0.4),
 	# call the checkUnbalanced function (defined below) every 2 seconds
--- doc/sphinx/tutorial/04-periodic-simple-shear.py
+++ doc/sphinx/tutorial/04-periodic-simple-shear.py
@@ -41,9 +41,9 @@ O.engines=[
 	ForceResetter(),
 	InsertionSortCollider([Bo1_Sphere_Aabb()]),
 	InteractionLoop(
-		[Ig2_Sphere_Sphere_L3Geom()],
+		[Ig2_Sphere_Sphere_ScGeom()],
 		[Ip2_FrictMat_FrictMat_FrictPhys()],
-		[Law2_L3Geom_FrictPhys_ElPerfPl()]
+		[Law2_ScGeom_FrictPhys_CundallStrack()]
 	),
 	NewtonIntegrator(damping=.4),
 	# run checkStress function (defined below) every second
