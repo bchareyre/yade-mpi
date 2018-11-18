9049c59306eb6a320bd7c4628ceb8a005c4f1721
bchareyre <bruno.chareyre@grenoble-inp.fr>
2018-11-19 00:47:00 +0100

    fix inspector crash and compile warnings

--- core/Subdomain.hpp
+++ core/Subdomain.hpp
@@ -42,7 +42,7 @@ class Subdomain: public Shape {
 	std::vector<double> getStateValues(unsigned otherSubdomain) { 
 		const shared_ptr<Scene>& scene= Omega::instance().getScene();
 		if (scene->subdomain==int(otherSubdomain)) {LOG_ERROR("subdomain cannot interact with itself"); return std::vector<double>();}
-		if (int(otherSubdomain) >= intersections.size()) {LOG_ERROR("otherSubdomain exceeds no. of subdomains ("<<otherSubdomain<<" vs. "<<intersections.size()); return std::vector<double>();}
+		if (otherSubdomain >= intersections.size()) {LOG_ERROR("otherSubdomain exceeds no. of subdomains ("<<otherSubdomain<<" vs. "<<intersections.size()); return std::vector<double>();}
 		const vector<Body::id_t>& search = intersections[otherSubdomain];
 		return getStateValuesFromIds(search);
 	}
@@ -116,13 +116,13 @@ class Subdomain: public Shape {
 		((Vector3r,boundsMax,Vector3r(NaN,NaN,NaN),,"max corner of all bboxes of members; differs from effective domain bounds by the extra length (sweepLength)"))
 		((IntersectionMap,intersections,IntersectionMap(),Attr::hidden,"[will be overridden below]"))
 		((IntersectionMap,mirrorIntersections,IntersectionMap(),Attr::hidden,"[will be overridden below]"))
-		((vector<Body::id_t>,ids,vector<Body::id_t>(),,"Ids of owned particles.")) //FIXME
-		((vector<vector<Real> >,stateBuffer,vector<vector<Real> >(),Attr::noSave,"container storing data from other subdomains")) 
+		((vector<Body::id_t>,ids,vector<Body::id_t>(),Attr::hidden,"Ids of owned particles.")) //FIXME
+		((vector<vector<Real> >,stateBuffer,vector<vector<Real> >(),(Attr::noSave | Attr::hidden),"container storing data from other subdomains")) 
 		,/*ctor*/ createIndex();
 		,/*py*/ /*.add_property("members",&Clump::members_get,"Return clump members as {'id1':(relPos,relOri),...}")*/
 		.def("setMinMax",&Subdomain::setMinMax,"returns bounding min-max based on members bounds. precondition: the members bounds have been dispatched already, else we re-use old values. Carefull if subdomain is not at the end of O.bodies.")
 		.def("getStateValues",&Subdomain::getStateValues,(boost::python::arg("otherDomain")),"returns pos,vel,angVel,ori of bodies interacting with a given otherDomain, based on :yref:`Subdomain.intersections`.")
-		.def("getStateValuesFromId",&Subdomain::getStateValuesFromIds,(boost::python::arg("b_ids")),"returns pos,vel,angVel,ori of listed bodies.")
+		.def("getStateValuesFromIds",&Subdomain::getStateValuesFromIds,(boost::python::arg("b_ids")),"returns pos,vel,angVel,ori of listed bodies.")
 		.def("setStateValuesFromIds",&Subdomain::setStateValuesFromIds,(boost::python::arg("b_ids"),boost::python::arg("input")),"set pos,vel,angVel,ori from listed body ids and data.")
 // 		.def("getStateValuesFromIdsAsArray",&Subdomain::getStateValuesFromIdsAsArray,(boost::python::arg("otherDomain")),"returns 1D numpy array of pos,vel,angVel,ori of bodies interacting with a given otherDomain, based on :yref:`Subdomain.intersections`.")
 		.def("setStateValuesFromBuffer",&Subdomain::setStateValuesFromBuffer,(boost::python::arg("subdomain")),"set pos,vel,angVel,ori from state buffer.")
