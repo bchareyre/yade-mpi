e247c4c13249d1e8bb270d8d69c3cfc397d9c28f
dpkn31 <deepak.kn1990@gmail.com>
2019-04-15 14:31:06 +0200

    serialize and deserialize a fake body container

--- pkg/mpi/MPIBodyContainer.cpp
+++ pkg/mpi/MPIBodyContainer.cpp
@@ -8,21 +8,21 @@ void MPIBodyContainer::clearContainer() {
 }
 
 
-string MPIBodyContainertoString(MPIBodyContainer mpiContainer) { 
-  
-    std::ostringstream oss; 
-    yade::ObjectIO::save<decltype(mpiContainer), boost::archive::binary_oarchive>(oss, "mpiContainer", mpiContainer); 
-    oss.flush(); 
-	return oss.str(); 
-  } 
-	
-
-void stringtoMPIBodyContainer(string mpiContainerString){ 	  
-  
-  MPIBodyContainer b; 	  
-  std::istringstream istr(mpiContainerString);  	
-  yade::ObjectIO::load<decltype(b),boost::archive::binary_iarchive>(istr,"mpiContainer",b);
-  std::cout << "size of b = "  << b.bContainer.size() << std::endl; 
-	
-} 
-	
+//string MPIBodyContainertoString(MPIBodyContainer mpiContainer) { 
+//  
+//    std::ostringstream oss; 
+//    yade::ObjectIO::save<decltype(mpiContainer), boost::archive::binary_oarchive>(oss, "mpiContainer", mpiContainer); 
+//    oss.flush(); 
+//	return oss.str(); 
+//  } 
+//	
+//
+//void stringtoMPIBodyContainer(string mpiContainerString){ 	  
+//  
+//  MPIBodyContainer b; 	  
+//  std::istringstream istr(mpiContainerString);  	
+//  yade::ObjectIO::load<decltype(b),boost::archive::binary_iarchive>(istr,"mpiContainer",b);
+//  std::cout << "size of b = "  << b.bContainer.size() << std::endl; 
+//	
+//} 
+//	
--- pkg/mpi/MPIBodyContainer.hpp
+++ pkg/mpi/MPIBodyContainer.hpp
@@ -1,82 +1,75 @@
-#pragma once 
+#pragma once
 
-#include <core/Body.hpp> 
+#include <core/Body.hpp>
 #include <core/Scene.hpp>
-#include<lib/base/Logging.hpp>
-#include <lib/serialization/Serializable.hpp> 
-#include<core/BodyContainer.hpp> 
+#include <core/BodyContainer.hpp>
 #include <core/Omega.hpp>
-#include<vector>
+#include <lib/base/Logging.hpp>
+#include <lib/serialization/Serializable.hpp>
+#include <vector>
 
-
-class MPIBodyContainer :  public Serializable{  
+class MPIBodyContainer :  public Serializable{
 
   public:
 
-    int procRank; // MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
-    
-    std::vector<shared_ptr<Body> > bContainer; // the container  
-        
+    int procRank; // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    void insertBody(int id) { 
-      const shared_ptr<Scene>& scene = Omega::instance().getScene(); 
-      shared_ptr<BodyContainer>& bodycontainer = scene-> bodies; 
-      shared_ptr<Body> b = (*bodycontainer)[id]; 
-      // if empty put body   
+    void insertBody(int id) {
+      const shared_ptr<Scene>& scene = Omega::instance().getScene();
+      shared_ptr<BodyContainer>& bodycontainer = scene-> bodies;
+      shared_ptr<Body> b = (*bodycontainer)[id];
+      // if empty put body
       if (bContainer.size() == 0 ){ bContainer.push_back(b); }
-      //check if body already exists 
+      //check if body already exists
       else {
-         int  c = 0; 
-        for (std::vector<shared_ptr<Body> >::iterator bodyIter=bContainer.begin(); bodyIter != bContainer.end(); ++bodyIter ){ 
- 	        const shared_ptr<Body>& inContainerBody = *(bodyIter); 
- 	        if (inContainerBody->id == b->id) {c += 1;} 
-       } if (c==0){bContainer.push_back(b); } 
-      } 
-    } 
-    
-    void clearContainer(); 
-    
-    virtual ~MPIBodyContainer(){}; 
-        
-    void insertBodyList(boost::python::list&  idList) {   
+         int  c = 0;
+        for (std::vector<shared_ptr<Body> >::iterator bodyIter=bContainer.begin(); bodyIter != bContainer.end(); ++bodyIter ){
+ 	        const shared_ptr<Body>& inContainerBody = *(bodyIter);
+ 	        if (inContainerBody->id == b->id) {c += 1;}
+       } if (c==0){bContainer.push_back(b); }
+      }
+    }
+
+    void clearContainer();
+
+    virtual ~MPIBodyContainer(){};
+
+    void insertBodyList(boost::python::list&  idList) {
       // insert a list of bodies
-      unsigned int listSize = boost::python::len(idList);  
-      for (unsigned int i=0; i != listSize; ++i) { 
+      unsigned int listSize = boost::python::len(idList);
+      for (unsigned int i=0; i != listSize; ++i) {
 	      int  b_id = boost::python::extract <int> (idList[i]);
-	      insertBody(b_id); 
-      } 	  
+	      insertBody(b_id);
+      }
     }
-    
-    unsigned int getCount() {  
-      return bContainer.size(); 
+
+    unsigned int getCount() {
+      return bContainer.size();
     }
-    
+
     shared_ptr<Body> getBodyat(int id){
-      return bContainer[id]; 
-    } 
-    
-    
-    void setBodiesToBodyContainer() { // to be used when deserializing a recieved container.  
-	    const shared_ptr<Scene>& scene = Omega::instance().getScene(); 
+      return bContainer[id];
+    }
+
+    void setBodiesToBodyContainer() { // to be used when deserializing a recieved container.
+	    const shared_ptr<Scene>& scene = Omega::instance().getScene();
 	    shared_ptr<BodyContainer>& bodyContainer = scene->bodies;
 	    for (std::vector<shared_ptr <Body>> ::iterator bIter = bContainer.begin(); bIter != bContainer.end(); ++ bIter) {
-	      shared_ptr<Body> newBody = *(bIter); 
-	      bodyContainer->insertAtId(newBody, newBody->id); 
-	    }	
-    } 
-    
-   
+	      shared_ptr<Body> newBody = *(bIter);
+	      bodyContainer->insertAtId(newBody, newBody->id);
+	    }
+    }
+  
   YADE_CLASS_BASE_DOC_ATTRS_CTOR_PY(MPIBodyContainer,Serializable,"a dummy container to serialize and send. ",
+  ((vector<shared_ptr<Body>>, bContainer,,,"a dummy body container to serialize"))
   ,
   ,
   .def("insertBody",&MPIBodyContainer::insertBody,(boost::python::arg("bodyId")),  "insert a body (by id) in this container")
   .def("insertBodyList", &MPIBodyContainer::insertBodyList, (boost::python::arg("listOfIds")), "inset a list of bodies (by ids)")
-  .def("clearContainer", &MPIBodyContainer::clearContainer, "clear bodies in the container") 
+  .def("clearContainer", &MPIBodyContainer::clearContainer, "clear bodies in the container")
   .def("getCount", &MPIBodyContainer::getCount, "get container count")
   .def("getBodyat", &MPIBodyContainer::getBodyat, "get body at id")
   );
   DECLARE_LOGGER;
-	
-}; 
-
-REGISTER_SERIALIZABLE(MPIBodyContainer); 
+};
+REGISTER_SERIALIZABLE(MPIBodyContainer);
